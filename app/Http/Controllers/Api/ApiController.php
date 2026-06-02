3ad6b66417c35de731010e3bbd8742417f1f8955
Jp-ctrl983 <joao.fernandes37.pedro@gmail.com>
2026-06-02 18:22:07 -0300

    criacoes completas

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,14 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    public function __construct(
+        private SeriesRepository $repository
+    ) {}
+
     public function index()
     {
         return Series::all();
@@ -22,12 +29,13 @@ class ApiController extends Controller
 
     public function upload(Request $request)
     {
-        $path = $request->query('path');
-    }
+        if ($request->hasFile('cover')) {
+            $coverPath = $request->file('cover')
+                ->store('series-cover', 'public');
+        }
 
-    public function show(string $id)
-    {
-        //
+        return response()
+            ->json(Series::all());
     }
 
     public function update(Request $request, string $id)
